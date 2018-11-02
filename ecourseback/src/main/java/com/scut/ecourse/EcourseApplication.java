d9116e13053e0fb08f8b49bd8084da08d6f66d96
liuxx <liux78@outlook.com>
2018-11-02 13:43:07 +0800

    init

---
+++ ecourseback/src/main/java/com/scut/ecourse/EcourseApplication.java
@@ -0,0 +1,13 @@
+package com.scut.ecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class EcourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcourseApplication.class, args);
+    }
+}
---
+++ ecourseback/src/main/java/com/scut/ecourse/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.scut.ecourse;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EcourseApplication.class);
+    }
+
+}
