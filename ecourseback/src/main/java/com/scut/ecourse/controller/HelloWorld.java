ea00405c17d163d44f5f99f4d5ed58feb6219daf
liuxx <liux78@outlook.com>
2018-11-10 13:40:26 +0800

    refactor: 改用前端设置代理的方式解决跨域问题

--- ecourseback/src/main/java/com/scut/ecourse/controller/HelloWorld.java
+++ ecourseback/src/main/java/com/scut/ecourse/controller/HelloWorld.java
@@ -13,18 +13,24 @@ public class HelloWorld {
     private Logger logger= LoggerFactory.getLogger(HelloWorld.class);
     @RequestMapping("/hello")
     public String hello() {
-        System.out.print("ok");
+        System.out.println("ok");
         return "Hello Spring Boot!";
     }
 
 
     /**
-     * @param user
+     * @param
      * @return 登录结果
      */
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String login(@RequestBody User user){
-        System.out.print(user.getUsername());
+   /* public String login(@RequestParam(value="username", required=true)String username,@RequestParam(value="password", required=true)String password){
+        System.out.println(username+" "+password);
+        return username;
+    }*/
+
+    public String login(User user){
+        System.out.println(user.getUsername());
+        System.out.println("hhh");
         return user.getUsername();
     }
 
