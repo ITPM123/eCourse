417c69a619c56c86ca6d494e4a257825074d1f76
Leonvdream <1016134287@qq.com>
2018-12-07 20:42:50 +0800

    Add files via upload

---
+++ ecourseback/src/main/java/com/scut/ecourse/controller/StudentController.java
@@ -0,0 +1,70 @@
+package com.scut.ecourse.controller;
+
+import com.scut.ecourse.entity.PersonEntity;
+import com.scut.ecourse.service.AcdemicDeanService;
+import com.scut.ecourse.service.StudentService;
+import com.scut.ecourse.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.HttpSessionRequiredException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/student")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String student(HttpServletRequest request) {
+        MultipartHttpServletRequest params = ((MultipartHttpServletRequest) request);
+
+        String personId = params.getParameter("personId");
+        System.out.println("personId:"+personId);
+        String username = params.getParameter("username");
+        System.out.println("username:"+username);
+        String password = params.getParameter("password");
+        System.out.println("password:"+password);
+        String sex = params.getParameter("sex");
+        System.out.println("sex:"+sex);
+        String school = params.getParameter("school");
+        System.out.println("school:"+school);
+        String realName = params.getParameter("realName");
+        System.out.println("realName:"+realName);
+        String code = params.getParameter("code");
+        System.out.println("code:"+code);
+        String title = params.getParameter("title");
+        System.out.println("title:"+title);
+        String major = params.getParameter("major");
+        System.out.println("major:"+major);
+        String grade = params.getParameter("grade");
+        System.out.println("grade:"+grade);
+        String classs = params.getParameter("classs");
+        System.out.println("classs:"+classs);
+        String address = params.getParameter("address");
+        System.out.println("address:"+address);
+        String email = params.getParameter("email");
+        System.out.println("email:"+email);
+        String motto = params.getParameter("motto");
+        System.out.println("motto:"+motto);
+
+        studentService.createStudent(Integer.parseInt(personId),username,password,Integer.parseInt(sex),school,
+                realName,code,title,major,grade,classs,address,email,motto);
+
+        return "upload successful";
+
+    }
+
+    @RequestMapping(value = "/removeStudent",method = RequestMethod.GET)
+    public Object removeStudent(int personId){
+        return studentService.removeStudent(personId);
+    }
+
+}
+
